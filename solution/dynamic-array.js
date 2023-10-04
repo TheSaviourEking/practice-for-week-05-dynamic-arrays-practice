91be49e16ccd91b18255f9a63b798fa43362f113
TheSaviourEking <saviour.eking@outlook.com>
2023-10-04 01:56:36 +0100

    add solutions

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,28 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
+  constructor(defaultSize = 4) {
 
     // Your code here
-
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
 
     // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
 
     // Your code here
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
+    return this.length;
   }
 
 }
